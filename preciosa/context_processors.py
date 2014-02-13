d8d23e9841969f77652acc0a613a3b7aa8037514
Martín Gaitán <gaitan@gmail.com>
2014-02-13 18:24:06 -0300

    excluir categoria "A CLASIFICAR" del menu

--- preciosa/context_processors.py
+++ preciosa/context_processors.py
@@ -2,4 +2,4 @@ from precios.models import Categoria
 
 
 def menu(request):
-    return {'menu': Categoria.objects.filter(depth=1)}
+    return {'menu': Categoria.objects.filter(depth=1).exclude(id=772)}
