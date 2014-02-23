ae6a43f5c20b590aa508c713f33b3a474148f82f
Martín Gaitán <gaitan@gmail.com>
2014-02-23 13:42:35 -0300

    unificando lo de nacho y lo poquito que habia bajo la url /api/v1/

--- preciosa/api/views.py
+++ preciosa/api/views.py
@@ -1,14 +1,35 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 
-from preciosa.precios.models import Sucursal
-from preciosa.precios.serializers import SucursalSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-@api_view(['GET',])
+from cities_light.models import City
+from preciosa.precios.models import Sucursal, Cadena
+
+from preciosa.precios.serializers import (CadenaSerializer, SucursalSerializer,
+                                          CitySerializer)
+
+# API
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class CadenaViewSet(viewsets.ModelViewSet):
+    queryset = Cadena.objects.all()
+    serializer_class = CadenaSerializer
+
+
+class SucursalViewSet(viewsets.ModelViewSet):
+    queryset = Sucursal.objects.all()
+    serializer_class = SucursalSerializer   # Create your views here.
+
+
+@api_view(['GET'])
 def sucursales_list(request):
     if request.method == 'GET':
         sucursales = Sucursal.objects.all()
@@ -17,9 +38,8 @@ def sucursales_list(request):
             point_str = [float(p) for p in request.GET.get('l').split('|')]
             point = Point(*point_str, srid=4326)
             distance = request.GET.get('d', 5)
-
-            sucursales = sucursales.filter(ubicacion__distance_lte=(point, D(km=distance)))
-
+            circulo = (point, D(km=distance))
+            sucursales = sucursales.filter(ubicacion__distance_lte=circulo)
 
         serializer = SucursalSerializer(sucursales, many=True)
         return Response(serializer.data)
