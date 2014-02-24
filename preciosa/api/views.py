185eb75664451ff700dc7a00057a631c1e6a3cdd
Martín Gaitán <gaitan@gmail.com>
2014-02-24 01:52:51 -0300

    migrando sucursales a APIViews. hack para inyectar urls custom en las que genera un default router

--- preciosa/api/views.py
+++ preciosa/api/views.py
@@ -1,9 +1,7 @@
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 
-from rest_framework import viewsets, mixins
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets, mixins, generics
 
 from cities_light.models import City
 from preciosa.precios.models import Sucursal, Cadena
@@ -25,7 +23,6 @@ class CreateListRetrieveViewSet(mixins.CreateModelMixin,
     pass
 
 
-
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = City.objects.filter(country__name='Argentina')
     serializer_class = CitySerializer
@@ -38,20 +35,37 @@ class CadenaViewSet(CreateListRetrieveViewSet):
 
 class SucursalViewSet(CreateListRetrieveViewSet):
     queryset = Sucursal.objects.all()
-    serializer_class = SucursalSerializer   # Create your views here.
+    serializer_class = SucursalSerializer
 
 
-@api_view(['GET'])
-def sucursales_list(request):
-    if request.method == 'GET':
-        sucursales = Sucursal.objects.all()
+class SucursalesList(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     generics.GenericAPIView):
 
-        if request.GET.get('l'):
-            point_str = [float(p) for p in request.GET.get('l').split('|')]
-            point = Point(*point_str, srid=4326)
-            distance = request.GET.get('d', 5)
-            circulo = (point, D(km=distance))
-            sucursales = sucursales.filter(ubicacion__distance_lte=circulo).distance(point).order_by('distance')
+    queryset = Sucursal.objects.all()
+    serializer_class = SucursalSerializer
 
-        serializer = SucursalSerializer(sucursales, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        queryset = super(SucursalesList, self).get_queryset()
+        lat = self.request.QUERY_PARAMS.get('lat', None)
+        lon = self.request.QUERY_PARAMS.get('lon', None)
+        radio = self.request.QUERY_PARAMS.get('radio', None)
+        if all((lat, lon, radio)):
+            try:
+                lat = float(lat)
+                lon = float(lon)
+                radio = float(radio)
+            except ValueError:
+                pass
+            else:
+                point = Point(lat, lon, srid=4326)
+                circulo = (point, D(km=radio))
+                queryset = queryset.filter(ubicacion__distance__lte=circulo)
+                queryset = queryset.distance(point).order_by('distance')
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
