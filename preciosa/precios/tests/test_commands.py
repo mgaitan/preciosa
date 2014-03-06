a0c0932ca771154bd70c31f6d6a975b9320f047d
Martín Gaitán <gaitan@gmail.com>
2014-03-06 11:39:21 -0300

    mas tests del wrapper de annalisa

--- preciosa/precios/tests/test_commands.py
+++ preciosa/precios/tests/test_commands.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from mock import patch
 from preciosa.precios.models import Producto
 from preciosa.precios.management.commands.annalisa import Annalisa
+from factories import MarcaFactory, CategoriaFactory
 
 
 class AnnalisaTestCase(TestCase):
@@ -20,21 +21,36 @@ class AnnalisaTestCase(TestCase):
         mock.get.return_value.json.return_value = json_response
 
     def test_unidad_volumen_litro_es_normalizada(self):
-        " Test my custom command."
         self.config({'unidad_volumen': 'litro'})
         r = self.api.analyze('')
         self.assertEqual(r['unidad_medida'], Producto.UM_L)
 
     def test_unidad_volumen_ml_es_normalizada(self):
-        " Test my custom command."
         self.config({'unidad_volumen': 'mililitro'})
         r = self.api.analyze('')
         self.assertEqual(r['unidad_medida'], Producto.UM_ML)
 
     def test_unidad_volumen_cc_es_normalizada(self):
-        " Test my custom command."
         self.config({'unidad_volumen': 'centimetro cubico'})
         r = self.api.analyze('')
         self.assertEqual(r['unidad_medida'], Producto.UM_ML)
+
+    def test_unidad_peso_gramo_normalizada(self):
+        self.config({'unidad_peso': 'gramo'})
+        r = self.api.analyze('')
+        self.assertEqual(r['unidad_medida'], Producto.UM_GRAMO)
+
+    def test_unidad_peso_kilo_normalizada(self):
+        self.config({'unidad_peso': 'gramo'})
+        r = self.api.analyze('')
+        self.assertEqual(r['unidad_medida'], Producto.UM_KILO)
+
+    def test_marca_es_normalizada(self):
+        marca = MarcaFactory()
+        self.config({'marcaid': marca.id})
+        r = self.api.analyze('')
+        self.assertEqual(r['marca'], marca)
+
+
 
 
