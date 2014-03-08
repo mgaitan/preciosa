d8c30e741c51e43382fbcd067763889f7b875eb5
Martín Gaitán <gaitan@gmail.com>
2014-03-08 19:46:23 -0300

    sucursales post acepta ubicacion

--- preciosa/api/views.py
+++ preciosa/api/views.py
@@ -117,7 +117,7 @@ class ProductosList(mixins.ListModelMixin,
             queryset = queryset.filter(upc__icontains=barcode)
 
         if q:
-            words = q.split(' ')
+            words = q.split()
             queryset = queryset.filter(
                 Q(reduce(operator.and_, (Q(descripcion__icontains=w) for w in words)))
                 #| Q(marca__nombre__icontains=q)
