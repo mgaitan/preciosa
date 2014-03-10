0e24ac4a8efcf303dd58fc438711b4d21fe33cc1
Martín Gaitán <gaitan@gmail.com>
2014-03-10 14:26:58 -0300

    empezando funciones utilitarias para saneary validar codigo de barra. ref #175

---
+++ tools/barcode.py
@@ -0,0 +1,44 @@
+"""
+utilidades para chequear codigos de barra
+"""
+
+def checksum(code):
+    """calcula el checksum para codigos
+    de barra segun algoritmo UPC/GTIN
+
+    NO GARANTIZA QUE UN CODIGO SEA VALIDO
+    / *no chequea longitud*
+    """
+
+    total = 0
+
+    for (i, c) in enumerate(code):
+        if i % 2 == 1:
+            total = total + int(c)
+        else:
+            total = total + (3 * int(c))
+
+    check_digit = (10 - (total % 10)) % 10
+    return str(check_digit)
+
+
+def normalizar(code):
+    """work in progress. """
+    largo = len(code)
+    if largo < 7:
+        # mayormente frutas y verduras. No es identificador
+        return None
+    if code.startswith('4000'):
+        # dummy code?  chequear.
+        # codigo interno del vendedor
+        return None
+    elif largo in (7, 12):
+        return code + checksum(code)
+    elif largo in (8, 13) and checksum(code[:-1]) == code[-1]:
+        return code
+
+
+
+
+
+
