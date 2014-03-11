2eb092672bce65b053f9f34e1af54c67f24670c4
diegoduncan21 <diegoduncan21@gmail.com>
2014-03-11 20:37:19 -0300

    issue #125 - fixing check if already subscribed

--- preciosa/accounts/forms.py
+++ preciosa/accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 import account.forms
 
+
 class SignupForm(account.forms.SignupForm):
     subscribe_to_newsletter = forms.BooleanField(
         required=False
--- preciosa/accounts/views.py
+++ preciosa/accounts/views.py
@@ -11,13 +11,14 @@ class SignupView(account.views.SignupView):
     def after_signup(self, form):
 
         if form.cleaned_data["subscribe_to_newsletter"]:
-            # subscribe_to_newsletter
 
-            Subscription.objects.create(
-                user=self.created_user,
-                newsletter=Newsletter.objects.first(),
-                subscribed=True
-            )
+            if not Subscription.objects.filter(
+                email_field=form.cleaned_data['email']
+            ).exists():
+                Subscription.objects.create(
+                    user=self.created_user,
+                    newsletter=Newsletter.objects.first(),
+                    subscribed=True
+                )
 
         super(SignupView, self).after_signup(form)
-
