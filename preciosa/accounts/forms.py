a8c40868618f4ce5e9886313a02283912929696d
diegoduncan21 <diegoduncan21@gmail.com>
2014-03-11 13:51:34 -0300

    adding subscribe to newsletter on signup form

---
+++ preciosa/accounts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+import account.forms
+
+class SignupForm(account.forms.SignupForm):
+    subscribe_to_newsletter = forms.BooleanField(
+        required=False
+    )
---
+++ preciosa/accounts/views.py
@@ -0,0 +1,23 @@
+from .forms import SignupForm
+
+from newsletter.models import Subscription, Newsletter
+
+import account.views
+
+
+class SignupView(account.views.SignupView):
+    form_class = SignupForm
+
+    def after_signup(self, form):
+
+        if form.cleaned_data["subscribe_to_newsletter"]:
+            # subscribe_to_newsletter
+
+            Subscription.objects.create(
+                user=self.created_user,
+                newsletter=Newsletter.objects.first(),
+                subscribed=True
+            )
+
+        super(SignupView, self).after_signup(form)
+
