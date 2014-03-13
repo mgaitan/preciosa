ad32e918f4ebb5bfa7d349342cd60752e7d1bb88
Martín Gaitán <gaitan@gmail.com>
2014-03-13 13:13:35 -0300

    factorizado el algoritmo de busqueda de productos como un método del manager. ref #170

--- preciosa/precios/views.py
+++ preciosa/precios/views.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-import operator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, DetailView
-from django.db.models import Q
+
 from django.http import HttpResponse
 from preciosa.precios.models import Producto, Categoria
-from tools import texto
 
 # Vistas web
 
@@ -49,28 +47,6 @@ def buscador(request):
     q = request.GET.get('q', '')
     if len(q) < 3:
         return HttpResponse('')
-
     context = {'q': q}
-    try:
-        int(q)
-        es_num = True
-    except ValueError:
-        es_num = False
-
-    if es_num:
-        productos = Producto.objects.filter(upc__startswith=q)[0:8]
-    else:
-        q = texto.normalizar(q)
-        words = q.split()
-
-        palabras = Q(reduce(operator.and_,
-                            (Q(busqueda__icontains=w) for w in words if len(w) > 2)))
-        tiene_palabras = Producto.objects.filter(
-            palabras).values_list('id', flat=True)
-        similares = Producto.objects.filter_o(busqueda__similar=q).values_list('id',
-                                                                               flat=True)
-        productos = Producto.objects.filter(Q(id__in=tiene_palabras) |
-                                            Q(id__in=similares)).distinct()[0:8]
-    context['productos'] = productos
-
+    context['productos'] = Producto.objects.buscar(q)
     return render(request, "precios/buscador.html", context)
