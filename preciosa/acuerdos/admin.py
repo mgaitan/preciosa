d7389dd0f4c22196ca8570edba2b084c00b3738a
Gonzalo Almeida <flecox@gmail.com>
2014-06-11 17:01:53 -0300

    enviando precios en acuerdo en el detalle y en mejores

--- preciosa/acuerdos/admin.py
+++ preciosa/acuerdos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Region, Acuerdo, PrecioEnAcuerdo
+
+admin.site.register(Region)
+admin.site.register(Acuerdo)
+admin.site.register(PrecioEnAcuerdo)
